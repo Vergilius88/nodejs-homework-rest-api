b57c9890bde0ef50677816070e7c42232f830da4
Nikolaienko Ihor <nikolasha8876@gmail.com>
2021-06-14 01:51:22 +0300

    prettier

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,35 +1,33 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const schemaCreateContact = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().required(),
-  phone: Joi.string().required()
+  phone: Joi.string().required(),
 });
 
 const schemaUpdateContact = Joi.object({
   name: Joi.string().alphanum().min(3).max(30).optional(),
   email: Joi.string().email().optional(),
-  phone: Joi.string().optional()
+  phone: Joi.string().optional(),
 });
 
 const validate = (schema, obj, next) => {
   const { error } = schema.validate(obj);
 
   if (error) {
-    const [{ message }]  = error.details;
+    const [{ message }] = error.details;
 
     return next({
       status: 400,
-      message:`Not valid data`
+      message: `Not valid data`,
     });
   }
   next();
-}
-module.exports.createContact = (req, res,next) => {
-
-  return validate(schemaCreateContact,req.body,next);
-}
+};
+module.exports.createContact = (req, res, next) => {
+  return validate(schemaCreateContact, req.body, next);
+};
 module.exports.updateContact = (req, res, next) => {
-
-  return validate(schemaUpdateContact,req.body,next)
-}
+  return validate(schemaUpdateContact, req.body, next);
+};
